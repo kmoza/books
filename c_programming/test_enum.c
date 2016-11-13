e1ccc802e5b1111c1a2a7302423313b75056b27c
Krishna Oza <krishoza15sep@gmail.com>
2016-11-13 20:23:16 +0530

    enum test

---
+++ c_programming/test_enum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+enum {ON,OFF};
+
+int main()
+{
+	//test passing enum to functions
+	/*
+	switch on = ON;
+
+	if(on == ON)
+	{
+		printf("The switch is on\n");
+	}
+	else
+		printf("The switch is off\n");
+
+	*/
+	return 0;
+}
