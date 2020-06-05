60c2212a9e8504286149e33961a860e4f0456c97
Krishna Oza <koza@nvidia.com>
2020-06-05 21:55:11 +0530

    options parsing using getopt_long

---
+++ linux_system_programming/advanced_linux_programming/ch02_writing_good_gnu_linux_software/exercise2_1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(int argc, char* argv[])
+{
+    printf("The name of this program is '%s'\n", argv[0]);
+    printf("This program was invoked with %d arguments\n", argc);
+
+    /*were any command line arguments specified?*/
+    if(argc>1)
+    {
+        /*yes print them*/
+        int i;
+        printf("The arguments are:\n");
+        for(i=1;i<argc;++i)
+        {
+            printf("%s\n", argv[i]);
+        }
+    }
+    return 0;
+}
